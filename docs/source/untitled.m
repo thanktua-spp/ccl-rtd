7e27c102344ffa40d85c4e1d982f53ea2de41e19
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2025-01-29 19:15:48 +0100

    updated figures for c#

--- docs/source/untitled.m
+++ docs/source/untitled.m
@@ -1,19 +1,19 @@
 % define the function handle
- dzdt = @(t,z) [z(2); -4*z(1)];
- 
- % set initial condition
- z0 = [0, 5];
-
- % set time span
- t_span = [0, 10];
- 
- % call the solver
- [T, Z] = ode45(dzdt, t_span, z0);
- 
- % display the result
- plot(T, Z, '-o');
- xlabel('t')
- ylabel('y')
- legend('y_1', 'y_2')
- title('Solving-SHO-with-Matlab-Ode45')
- saveas(gcf, 'Solving-SHO-with-Matlab-Ode45', 'png')
+         a = 0.25;
+         dydt = @(t,y) 2*(a - t)*y^2;
+         
+         % set initial condition
+         y0 = 15.9;
+         
+         % set time span
+         t_span = [0, 1];
+         
+         % call the solver
+         [T, Y] = ode23(dydt, t_span, y0);
+         
+         % display the result
+         plot(T, Y, '-o');
+         xlabel('t')
+         ylabel('y')
+         title('Solving-with-Matlab-Ode23')
+         saveas(gcf, 'Solving-with-Matlab-Ode23', 'png')
