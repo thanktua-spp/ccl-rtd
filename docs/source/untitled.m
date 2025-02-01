ee7e81f7139d2f1dba1e47ad117d8e8aef7524de
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2025-02-01 18:10:58 +0100

    lots of changes

--- docs/source/untitled.m
+++ docs/source/untitled.m
@@ -1,4 +1,6 @@
 function [T, Y] = HowarthTransform(M)
+
+    % define parameters, functions anf their derivatives
     gamma = 1.4; 
     Pr = 0.7; 
     C = Pr*(gamma - 1)*M^2;
@@ -8,34 +10,51 @@ function [T, Y] = HowarthTransform(M)
     dmudh = @(h) (2/3)*h^(-1/3);
     rhomu = @(h)rho(h)*mu(h);
     drhomu = @(h, dh) (rho(h)*dmudh(h) + drhodh(h)*mu(h))*dh;
+
+    % define the differential equation
     function dy = dydt(~, y)
-        rhomu_h = rhomu(y(4)); drhomu_h = drhomu(y(4), y(5));
-        dy = [y(2); y(3); -(2*drhomu_h + y(1))*y(3)/(2*rhomu_h); y(5);
+
+        rhomu_h = rhomu(y(4)); 
+        drhomu_h = drhomu(y(4), y(5));
+        dy = [y(2); 
+              y(3); 
+              -(2*drhomu_h + y(1))*y(3)/(2*rhomu_h); y(5);
               -(drhomu_h*y(5) + Pr*y(1)*y(5) + C*rhomu_h*y(3)^2)/rhomu_h];
+
     end
-    
-    tspan = [0,5]; 
-    T = tspan; 
-    Y = tspan;
-    
+
+    % set time span and intial guess
+    tspan = [0, 5]; 
+    y35guess = [0.5; 1];
+
+    % define the nonlinear system to compute the initial condition
     function res = fun(y35_0)
+
         y0 = [0, 0, y35_0(1), 2, y35_0(2)];
         [T, Y] = ode45(@dydt, tspan, y0);
         res = Y(end, [2,4])' - 1;
+
     end
     
-    fsolve(@fun, [0.5;1]);
+    % solve for the unknown initial conditions
+    fsolve(@fun, y35guess);
     Y(:,4) = Y(:,4)-1;
 end
 
+% generator solution for M = 5 and plot
 [T, Y] = HowarthTransform(0); 
 figure(color='w'); hold on; 
 plot(T, Y(:, 2), 'b', linewidth = 2);
 plot(T, Y(:, 4), 'r', linewidth = 2);
 
-
+% generator solution for M = 5 and plot
 [T, Y] = HowarthTransform(5);
 plot(T, Y(:, 2), 'b', linewidth = 2);
 plot(T, Y(:, 4), 'r', linewidth = 2);
+
+% add legend, axis label and title
+legend("f'", "h-1");
+xlabel("η"); 
+title("Howarth Transformation");
 axis([0,5,0,2]); box on;
-saveas(gcf, 'Howarth-Transformation-Matlab', 'png')
+saveas(gcf, 'Howarth-Transformation-Matlab', 'png');
