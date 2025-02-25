d76236ee49714ae02addb4a188fb9083c8da3dce
Lateef Kareem <lateef.a.kareem@cyphercrescent.com>
2025-02-25 18:10:35 +0100

    grid c#

--- docs/source/untitled2.m
+++ docs/source/untitled2.m
@@ -1,48 +1,58 @@
-% define zfunction
-function crtrhy =  CrTrHY(Pr, Tr)
+% define the solution in time space
+function wt = EdgeClosedBoundaryRadial_Wd(tD, rD)
 
-    % define density equation
-    function yres = yfunc(y, A, B, C, D, Pr)
-         y2 = y * y; y3 = y2 * y; y4 = y3 * y; Den = (1 - y)^3;
-         yres = -A * Pr + (y + y2 + y3 - y4) / Den - B * y2 + C * y^D;
-    end
+   % define the solution in laplace space
+   function ws = lapW(s)
+      sqrts = sqrt(s); sqrts3 = s * sqrts; rDsqrts = rD * sqrts;
+      Num = besseli(1, rDsqrts) * besselk(1, sqrts) - besselk(1, rDsqrts) * besseli(1, sqrts);
+      Den = besseli(1, rDsqrts) * besselk(0, sqrts) + besselk(1, rDsqrts) * besseli(0, sqrts);
+      if (isinf(Num) || isinf(Den))
+         ws = 1 / sqrts3;
+      else
+         ws = Num / (sqrts3 * Den);
+      end
+   end
 
-    % avoid computing intermediate variables.
-    t = 1 / Tr; t2 = t * t; t3 = t2 * t;
-    tm1 = 1 - t; tm1e2 = tm1 * tm1;
-    A = 0.06125 * t * exp(-1.2 * tm1e2);
-    B = 14.76 * t - 9.76 * t2 + 4.58 * t3;
-    C = 90.7 * t - 242.2 * t2 + 42.4 * t3;
-    D = 2.18 + 2.82 * t; r = A * Pr;
-    if(Pr > 15)
-        r = r/2;
-    end
-
-    % solve the density equation
-    y = fsolve(@(y) yfunc(y, A, B, C, D, Pr), r);
-
-    % compute crtr
-    y2 = y * y; y3 = y2 * y; y4 = y3 * y; ym1p4 = (1 - y)^4; yDm1 = y^(D - 1);
-    Den = (1 + 4 * y + 4 * y2 - 4 * y3 + y4) / ym1p4 - 2 * B * y + C * D * yDm1;
-   crtrhy = A*Tr/(y*Den);
-
+   if(tD == 0 || rD == 1)
+      wt =  0;
+   else
+      wt = niLaplace(@(s)lapW(s), tD);
+   end
 end
 
-% set up ressure and temperature mesh
-Pr = linspace(0.2, 25, 501);
-Tr = [1.05,  1.1,  1.2,  1.3,  1.4];
+% define the time and radial mesh
+Rd = [2, 2.5, 3, 3.5, 4, 50];
+Td = logspace(-1, 2);
 
-% compute z factors and plot them
-figure(Color = 'w'); Tlabels = {};
-for tr = Tr
-    loglog(Pr, arrayfun(@(pr) CrTrHY(pr, tr), Pr)); hold on;
-    Tlabels = [Tlabels, "Tr = " + tr];
+% compute the water influx and plot
+figure(Color='w');
+lgd = {};
+for rD = Rd
+   Wd = arrayfun(@(tD)EdgeClosedBoundaryRadial_Wd(tD, rD), Td);
+   semilogx(Td, Wd, linewidth = 2); hold on;
+   lgd = [lgd, "rD = " + num2str(rD)];
 end
+lgd(end) = 'rD = ∞'; grid on;
+xlabel("tD"); ylabel("WD");
+legend(lgd, location = "northwest")
+axis([0.1, 100, 1, 8]);
+title("Dimensionless Water Influx");
+saveas(gcf, "Dimensionless-Water-Influx-Matlab-4dn.png");
 
-% add legend, axis label and title
-legend(Tlabels, location = "southwest");
-xlabel("Pr"); ylabel("CrTr"); 
-axis([0.1, 100, 0.01, 10]);
-grid on; grid minor;
-title("Reduced Compressibility Hall Yarborough"); box on;
-saveas(gcf, 'Reduced-Compressibility-Hall-Yarborough-Matlab', 'png');
+% define the time and radial mesh
+Rd = [5,6,7,8,9,10, 50];
+Td = logspace(0, 3);
+
+% compute the water influx and plot
+figure(Color='w'); lgd = {};
+for rD = Rd
+   Wd = arrayfun(@(tD)EdgeClosedBoundaryRadial_Wd(tD, rD), Td);
+   semilogx(Td, Wd, linewidth = 2); hold on;
+   lgd = [lgd, "rD = " + num2str(rD)];
+end
+lgd(end) = 'rD = ∞'; grid on;
+xlabel("tD"); ylabel("WD");
+legend(lgd, location = "northwest")
+axis([1,1000,0, 70]);
+title("Dimensionless Water Influx");
+saveas(gcf, "Dimensionless-Water-Influx-Matlab-5up.png");
